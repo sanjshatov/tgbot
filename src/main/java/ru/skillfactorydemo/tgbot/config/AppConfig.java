ef33402b647c8a15f40d267c95fc6fca784e1674
ваше имяAlexander <sanj12@ya.ru>
2022-12-21 20:39:27 +0500

    Initial commit

---
+++ src/main/java/ru/skillfactorydemo/tgbot/config/AppConfig.java
@@ -0,0 +1,47 @@
+package ru.skillfactorydemo.tgbot.config;
+
+import jakarta.xml.soap.MessageFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.oxm.jaxb.Jaxb2Marshaller;
+import org.springframework.web.filter.CharacterEncodingFilter;
+import org.springframework.ws.soap.saaj.SaajSoapMessageFactory;
+import ru.skillfactorydemo.tgbot.dto.GetCursOnDateXml;
+import ru.skillfactorydemo.tgbot.dto.GetCursOnDateXmlResponse;
+import ru.skillfactorydemo.tgbot.dto.GetCursOnDateXmlResult;
+import ru.skillfactorydemo.tgbot.dto.ValuteCursOnDate;
+import ru.skillfactorydemo.tgbot.service.CentralRussianBankService;
+
+import javax.xml.soap.SOAPConstants;
+import java.nio.charset.StandardCharsets;
+
+@Configuration
+public class AppConfig {
+
+    @Bean
+    public CentralRussianBankService cbrService() throws jakarta.xml.soap.SOAPException {
+        CentralRussianBankService cbrService = new CentralRussianBankService();
+        Jaxb2Marshaller jaxb2Marshaller = new Jaxb2Marshaller();
+        MessageFactory msgFactory = MessageFactory.newInstance(SOAPConstants.SOAP_1_2_PROTOCOL);
+        SaajSoapMessageFactory newSoapMessageFactory = new SaajSoapMessageFactory(msgFactory);
+        cbrService.setMessageFactory(newSoapMessageFactory);
+
+        jaxb2Marshaller.setClassesToBeBound(
+                GetCursOnDateXml.class,
+                GetCursOnDateXmlResponse.class,
+                GetCursOnDateXmlResult.class,
+                ValuteCursOnDate.class);
+
+        cbrService.setMarshaller(jaxb2Marshaller);
+        cbrService.setUnmarshaller(jaxb2Marshaller);
+        return cbrService;
+    }
+
+    @Bean
+    public CharacterEncodingFilter characterEncodingFilter() {
+        CharacterEncodingFilter filter = new CharacterEncodingFilter();
+        filter.setEncoding(StandardCharsets.UTF_8.name());
+        filter.setForceEncoding(true);
+        return filter;
+    }
+}
---
+++ src/main/java/ru/skillfactorydemo/tgbot/dto/GetCursOnDateXml.java
@@ -0,0 +1,19 @@
+package ru.skillfactorydemo.tgbot.dto;
+
+import lombok.Data;
+
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlRootElement;
+import javax.xml.datatype.XMLGregorianCalendar;
+
+
+@XmlRootElement(name = "GetCursOnDateXML", namespace = "http://web.cbr.ru/")
+@XmlAccessorType(XmlAccessType.FIELD)
+@Data //Геттеры и сеттеры
+public class GetCursOnDateXml {
+
+    @XmlElement(name = "On_date", required = true, namespace = "http://web.cbr.ru/") //Указание на то, в каком теге XML должно быть данное поле
+    protected XMLGregorianCalendar onDate;
+}
